9ef384e4429cd684bd7a85a6eb34d2e6a9d3a40d
lopezjuanma96 <lopezjuanma96@gmail.com>
2022-03-05 16:09:09 -0300

    entrega 1

---
+++ notes/mern.js
@@ -0,0 +1,18 @@
+/*
+MERN STACK
+
+MongoDB - Base de Datos
+Express - Desarrollo Back End
+React/ReactNative - Desarrollo Front End
+Node - Entorno de desarrollo
+
+Un setup de full stack de desarrollo. Tanto React, Express y Node son FrameWorks de Js
+Mongo es una base de datos no relacional, por lo que usa objetos lo cual pega muy bien con JS
+O sea que MERN es básicamente un Stack muy orientado a usar JS
+
+ESCALABILIDAD HORIZONTAL: Particionar el sistema de procesamiento en varios servidores para mejorar el Procesado
+ESCALABILIDAD VERTICAL: Mejorar la estructura de un solo servidor para mejorar el Procesado
+
+Hoy se pioriza mucho la escalabilidad horizontal, MongoDB es compatible con esto
+
+*/
