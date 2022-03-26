3da2984402c6e84f3e980fb6762b9f12188b9c1a
lopezjuanma96 <lopezjuanma96@gmail.com>
2022-03-26 03:57:07 -0300

    for entrega3: - created contenedorClass as a module to export the class Contenedor to other scripts - on main imported contenedorClass and express modules to implement a server with requests

---
+++ contenedorClass.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+
+module.exports = class Contenedor {
+
+    filePath;
+    fileString;
+
+    constructor(_filePath){
+        this.filePath = _filePath || "./container.json";
+    }
+
+    _getNewId(arr){
+        let aux = 0;
+        arr.forEach((elem) => {
+            if (elem > aux){
+                aux = elem;
+            }
+        })
+        return aux + 1;
+    }
+    
+    async save(val){
+        try{
+            const contentString = await fs.promises.readFile(this.filePath, 'utf-8');
+            const content = JSON.parse(contentString);
+            const newId = this._getNewId(content.map((elem) => elem.id));
+            content.push({...val, id:newId});
+            await fs.promises.writeFile(this.filePath, JSON.stringify(content));
+            console.log("Nuevo valor creado");
+        } catch (error) {
+            throw new Error(`Error al intentar crear el nuevo valor:\n${error}`);
+        }
+    }
+
+    getAll(){
+        try{
+            return JSON.parse(fs.readFileSync(this.filePath, 'utf-8'));
+        } catch (error) {
+            throw new Error(`Error leyendo los valores:\n${error}`);
+        }
+    }
+
+    getById(id){
+        const arr = this.getAll();
+        const search = arr.find((elem)=> elem.id === id);
+        if(search){
+            return search;
+        } else {
+            throw new Error("No se encontro el id solicitado");
+        }
+    }
+
+    async deleteAll(){
+        try{
+            prom = await fs.promises.unlink(this.filePath);
+            console.log("Todos los valores fueron borrados");
+        } catch (error) {
+            throw new Error(`Error al intentar borrar todos los datos:\n${error}`);
+        }
+    }
+
+    async deleteById(id){
+        try{
+            const arr = this.getAll();
+            const newArr = arr.filter((elem) => elem.id != id);
+            await fs.promises.writeFile(this.filePath, JSON.stringify(newArr));
+            console.log(`El elemento con id ${id} fue eliminado correctamente`);
+        } catch (error) {
+            throw new Error(`Error al intentar borrar el elemento con id ${id}:\n${error}`);
+        }
+        
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,27 @@
+const Contenedor = require('./contenedorClass.js')
+const express = require('express')
+const app = express()
+const cont = new Contenedor('productosCont.json')
+const PORT = 8080
+
+const getRandom = () => {
+    const contArr = cont.getAll();
+    const rand = contArr.length * Math.random();
+    const selection = parseInt(String(rand));
+    return contArr[selection];
+}
+
+const server = app.listen(PORT, () => {console.log(`Servidor abierto en puerto ${PORT}`)})
+app.on('error', (err) => {console.log(`Error en la carga del servidor:\n${err}`)})
+
+app.get('/', (req, res) => {
+    res.send('<ul><li><a href="./productos">Lista de Productos</a></li><li><a href="./productoRandom">Producto al Azar</a></li></ul>');
+})
+
+app.get('/productos', (req, res) => {
+    res.send(cont.getAll());
+})
+
+app.get('/productoRandom', (req, res) => {
+    res.send(getRandom());
+})
