013e1ab7ecb31834994706321de51027ad1ac1c2
Juanma López <lopezjuanma96@gmail.com>
2022-07-17 14:32:03 -0300

    finished missing TOKEN and passport implement bug on messages when changing users

--- utils/loginMethods.js
+++ utils/loginMethods.js
@@ -1,18 +1,24 @@
 import usersDAO from '../daos/users/usuariosDAOMongoAtlas.js';
+import bCrypt from 'bcrypt';
 
 const users = new usersDAO();
 
+//MANUALLY DONE
 export async function createNewUser(data){
     const userExists = await users.getById(data.alias);
     if(userExists){
         throw new Error("User already exists")
     } else {
-        await users.save(data, data.alias); //HERE add password hashing
+        await users.save({...data, pass: doHash(data.pass)}, data.alias);
     }
 }
 
 function checkPassword(a, b){
-    return a === b; //HERE add hashing check
+    return  bCrypt.compareSync(a, b);
+}
+
+function doHash(pass) {
+    return bCrypt.hashSync(pass, bCrypt.genSaltSync(10), null);
 }
 
 export async function loginUser(data){
@@ -30,3 +36,5 @@ export async function loginUser(data){
         throw new Error("Invalid Credentials");
     }
 }
+
+//FOR LAST PROJECT: DO IT WITH PASSPORT AND USE ALIAS AS _id NOT id (mongoDb document id NOT a key inside the document called id)
