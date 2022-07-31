43d03c4a0477f19033b5b5ac19653d4f3a999e13
Juanma López <lopezjuanma96@gmail.com>
2022-07-31 10:08:26 -0300

    security&owasp lesson

---
+++ notes/security&owasp.js
@@ -0,0 +1,9 @@
+//OWASP: Open Source project that analyzes the security vulnerabilities of webs 
+//and apps to ensure safe navigation and recomend things to do in your own projects
+//https://owasp.org/
+//Cada cierto tiempo hacen el OWASP TOP 10 con los mayores threats the ese año
+
+/*
+Hoy en día se especifica mucho el desarrollo, por lo que hay personas deddicadas 
+a la seguridad de las aplicaciones
+*/
