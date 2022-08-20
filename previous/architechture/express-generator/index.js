a1df21f52322eb0e679b6e5868abce7f7ae6bb84
Juanma López <lopezjuanma96@gmail.com>
2022-08-20 12:45:33 -0300

    express-generator

---
+++ previous/architechture/express-generator/index.js
@@ -0,0 +1,4 @@
+//express generator is installed globally to use it from cli
+//commands: express --views=hbs prepares an app for use of handlebars views
+//to run apps created with express generator, dont use node, but rather npm commands
+//where you have debug and morgan mode, which log different things
